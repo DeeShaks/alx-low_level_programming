0d74c0604c5546765cc2c3f10d2a6c6e5e760457
deeshaki <deeshaki1212@gmail.com>
2023-02-17 13:17:16 +0300

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Serves as the entry point for the program
  * Return: 0 (Success)
  */
-int main (void)
-{	
-	puts("\"Programming is like a multilingual puzzle");
-	return(0);
+int main(void)
+{
+puts("\"Programming is like a multilingual puzzle");
+return (0);
 }
