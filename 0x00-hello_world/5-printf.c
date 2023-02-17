d2b0be7a096c3466e3e938f1b6e122be5d084f52
deeshaki <deeshaki1212@gmail.com>
2023-02-17 13:26:36 +0300

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - main entry of our source
+*description - print to the stdout using printf
+*return - always be zero to show success
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\");
+return (0);
+}
