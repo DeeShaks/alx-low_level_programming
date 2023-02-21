7ba25440a476a6ab16742781bf78a61ac80ce5b0
deeshaki <deeshaki1212@gmail.com>
2023-02-21 12:20:04 +0300

    Small change on the files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+  char i;
+  int j;
+  for(j=0;j<10;j++)
+    {
+      for(i='a';i<='z';i++)
+	_putchar(i);
+      _putchar('/n');
+    }
+}
