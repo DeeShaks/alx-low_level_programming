bfe2e6c5956eb2f51ee836c545d45e207cb7a4cf
deeshaki <deeshaki1212@gmail.com>
2023-02-22 11:23:44 +0300

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - testing the function that prints if the int is +ve or -ve
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
